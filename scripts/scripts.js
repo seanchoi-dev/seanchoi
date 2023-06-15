5910f501a0e8f0b92d64a239eeda60618b206aed
Sean Choi <seanchoi@adobe.com>
2023-06-15 13:58:47 -0600

    change libs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ import { setLibs } from './utils.js';
 const STYLES = '';
 
 // Use '/libs' if your live site maps '/libs' to milo's origin.
-const LIBS = 'https://milo.adobe.com/libs';
+const LIBS = '/libs';
 
 // Add any config options.
 const CONFIG = {
